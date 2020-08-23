610ea3829af0dc78f765331cf768741fbd01ddae
Steve Roday <srrodrig.rodriguez@gmail.com>
2020-08-22 19:10:44 -0500

    Updated js file to set and get from local storage, added a little styling to page

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     //Current date for Header
+    
     var currDate = moment().format("dddd, MMMM Do");
     $("#currentDay").append(currDate);
-
-    //Grab current hour value
+   
+    // Grab current hour value
     var currHr = moment().hour();
     console.log(currHr);
     
@@ -48,27 +49,135 @@ $(document).ready(function() {
     
     colorCode();
     
+   
     //Get save button to save input field text to loc storage
-    var storText = document.getElementById("schedule");
-    $("#saveBtn").click(function (e) {
+    $("#saveBtn9").click(function (e) {
         e.preventDefault();
         // Create variable to grab input text and push to array
-        var schedItem = $(".toDo").get().map(function(el) {return el.value});
-        console.log(schedItem);
+        var schedItem = $("#nueve").get().map(function(el) {return el.value});
+        //console.log(schedItem)
         // Save text to local storage
         localStorage.setItem(schedule, JSON.stringify(schedItem));
-
     });
+    // Retrieve from local storage
+    function retrieve9(schedLog) {
+        var schedLog = JSON.parse(localStorage.getItem(schedule));
+        $.each(schedLog, function (index, value) {
+           // console.log(schedLog)
+        $("#nin").append(value);
+        });
+    };
+    $("#saveBtn10").click(function (e) {
+        e.preventDefault();
+        // Create variable to grab input text and push to array
+        var schedItem = $("#diez").get().map(function(el) {return el.value});
+        //console.log(schedItem);
+        // Save text to local storage
+        localStorage.setItem("#diez", JSON.stringify(schedItem));
+    });
+    // Retrieve from local storage
+    function retrieve10(schedLog10) {
+        var schedLog10 = JSON.parse(localStorage.getItem("#diez"));
+        $.each(schedLog10, function (index, value) {
+            console.log(schedLog10)
+        $("#ten").append(value);
+        });
+    };
+    $("#saveBtn11").click(function (e) {
+        e.preventDefault();
+        // Create variable to grab input text and push to array
+        var schedItem = $("#once").get().map(function(el) {return el.value});
+        // Save text to local storage
+        localStorage.setItem("#once", JSON.stringify(schedItem));
+    });
+    // Retrieve from local storage
+    function retrieve11(schedLog11) {
+        var schedLog11 = JSON.parse(localStorage.getItem("#once"));
+        $.each(schedLog11, function (index, value) {
+        $("#ele").append(value);
+        });
+    };
+    $("#saveBtn12").click(function (e) {
+        e.preventDefault();
+        // Create variable to grab input text and push to array
+        var schedItem = $("#doce").get().map(function(el) {return el.value});
+        // Save text to local storage
+        localStorage.setItem("#doce", JSON.stringify(schedItem));
+    });
+    // Retrieve from local storage
+    function retrieve12(schedLog12) {
+        var schedLog12 = JSON.parse(localStorage.getItem("#doce"));
+        $.each(schedLog12, function (index, value) {
+        $("#twe").append(value);
+        });
+    };
+    $("#saveBtn13").click(function (e) {
+        e.preventDefault();
+        // Create variable to grab input text and push to array
+        var schedItem = $("#trece").get().map(function(el) {return el.value});
+        // Save text to local storage
+        localStorage.setItem("#trece", JSON.stringify(schedItem));
+    });
+    // Retrieve from local storage
+    function retrieve13(schedLog13) {
+        var schedLog13 = JSON.parse(localStorage.getItem("#trece"));
+        $.each(schedLog13, function (index, value) {
+        $("#thi").append(value);
+        });
+    };
+    $("#saveBtn14").click(function (e) {
+        e.preventDefault();
+        // Create variable to grab input text and push to array
+        var schedItem = $("#catorce").get().map(function(el) {return el.value});
         
+        // Save text to local storage
+        localStorage.setItem("#catorce", JSON.stringify(schedItem));
+    });
+    // Retrieve from local storage
+    function retrieve14(schedLog14) {
+        var schedLog14 = JSON.parse(localStorage.getItem("#catorce"));
+        $.each(schedLog14, function (index, value) {
+        $("#fou").append(value);
+        });
+    };
+    $("#saveBtn15").click(function (e) {
+        e.preventDefault();
+        // Create variable to grab input text and push to array
+        var schedItem = $("#quince").get().map(function(el) {return el.value});
+        // Save text to local storage
+        localStorage.setItem("#quince", JSON.stringify(schedItem));
+    });
+    // Retrieve from local storage
+    function retrieve15(schedLog15) {
+        var schedLog15 = JSON.parse(localStorage.getItem("#quince"));
+        $.each(schedLog15, function (index, value) {
+        $("#fif").append(value);
+        });
+    };
+    $("#saveBtn16").click(function (e) {
+        e.preventDefault();
+        // Create variable to grab input text and push to array
+        var schedItem = $("#diezyseis").get().map(function(el) {return el.value});
+        // Save text to local storage
+        localStorage.setItem("#diezyseis", JSON.stringify(schedItem));
+    });
+    // Retrieve from local storage
+    function retrieve16(schedLog16) {
+        var schedLog16 = JSON.parse(localStorage.getItem("#diezyseis"));
+        $.each(schedLog16, function (index, value) {
+        $("#six").append(value);
+        });
+    };
+    retrieve9();
+    retrieve10();
+    retrieve11();
+    retrieve12();
+    retrieve13();
+    retrieve14();
+    retrieve15();
+    retrieve16();
 
     
 });
 
- /*var data = $("#schedule").serializeArray();
-        console.log(data);
 
-        $.each(data, function(i, obj) {
-            console.log(i, obj);
-            localStorage.setItem(obj.text);*/
-
-        //localStorage.setItem("schedule", storText.value);
